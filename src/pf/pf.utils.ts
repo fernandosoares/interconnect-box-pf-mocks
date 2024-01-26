59d0466f6250f9c0013cdf571b32a89b6c59a618
Fernando Soares <fernando.soares@boavistascpc.com.br>
2024-01-26 09:09:34 -0300

    fix: documento Breno

--- src/pf/pf.utils.ts
+++ src/pf/pf.utils.ts
@@ -8,26 +8,29 @@ export class PfUtils {
       case '83332845071':
         return {
           documento: doc,
-          nome: "Alice Gomes Pinto",
-          situacaoDocumento: "regular",
-          recomendacao: "avaliar",
-          informacoes: "Considerando a validação das informações pertencentes a essa política da base de dados da Boa Vista SCPC, recomendamos não seguir com esta negociação."
+          nome: 'Alice Gomes Pinto',
+          situacaoDocumento: 'regular',
+          recomendacao: 'avaliar',
+          informacoes:
+            'Considerando a validação das informações pertencentes a essa política da base de dados da Boa Vista SCPC, recomendamos não seguir com esta negociação.',
         };
       case '99330587038':
         return {
           documento: doc,
-          nome: "Victor Rodrigues Oliveira",
-          situacaoDocumento: "irregular",
-          recomendacao: "reprovar",
-          informacoes: "Considerando a validação das informações pertencentes a essa política da base de dados da Boa Vista SCPC, recomendamos não seguir com esta negociação."
+          nome: 'Victor Rodrigues Oliveira',
+          situacaoDocumento: 'irregular',
+          recomendacao: 'reprovar',
+          informacoes:
+            'Considerando a validação das informações pertencentes a essa política da base de dados da Boa Vista SCPC, recomendamos não seguir com esta negociação.',
         };
-      case 'Breno Fernandes Melo':
+      case '51138164003':
         return {
           documento: doc,
-          nome: "Victor Rodrigues Oliveira",
-          situacaoDocumento: "regular",
-          recomendacao: "indisponivel",
-          informacoes: "Considerando a validação das informações pertencentes a essa política da base de dados da Boa Vista SCPC, recomendamos não seguir com esta negociação."
+          nome: 'Breno Fernandes Melo',
+          situacaoDocumento: 'regular',
+          recomendacao: 'indisponivel',
+          informacoes:
+            'Considerando a validação das informações pertencentes a essa política da base de dados da Boa Vista SCPC, recomendamos não seguir com esta negociação.',
         };
       default:
         return null;
